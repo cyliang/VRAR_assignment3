56a354afdc67e496f1ddb0015a0c3fe3816b6362
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-05-11 22:21:20 +0800

    Cat's behavior/animation & controller to show cats

---
+++ Assets/Scripts/CatBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatBehavior : MonoBehaviour {
+
+    public Vector3 finalPosition { get; set; }
+    public bool captured { get; set; }
+    public float runSpeed;
+
+    private bool arrived;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        arrived = false;
+        captured = false;
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (captured && !arrived)
+            running();
+        else
+            idle();
+	}
+
+    void idle() {
+        animator.SetBool("Run", false);
+    }
+
+    void running() {
+        if (finalPosition == transform.position) {
+            arrived = true;
+            return;
+        }
+
+        animator.SetBool("Run", true);
+        transform.LookAt(finalPosition);
+        transform.position = Vector3.MoveTowards(transform.position, finalPosition, runSpeed);
+    }
+}
---
+++ Assets/Scripts/CatsController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CatsController : MonoBehaviour {
+
+    public GameObject catStartTemplate;
+    public Transform cameraTransform;
+    public Vector3 defaultFinalPosition;
+    public Vector3 finalPositionInterval;
+
+    private List<CatBehavior> cats = new List<CatBehavior>();
+
+	// Use this for initialization
+	void Start () {
+        randomCreateCat();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void randomCreateCat() {
+        CatBehavior newCat = (Instantiate(catStartTemplate, 
+                                new Vector3(Random.Range(5f, 10f), catStartTemplate.transform.position.y, Random.Range(5f, 10f)) + cameraTransform.position, 
+                                new Quaternion(0, Random.Range(0f, 360f), 0, 0)
+                             ) as GameObject).GetComponent<CatBehavior>();
+        newCat.finalPosition = defaultFinalPosition + finalPositionInterval * cats.Count;
+        newCat.transform.parent = transform;
+        cats.Add(newCat);
+    }
+}
