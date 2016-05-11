fcb9d4b354049b0d5ab3d3334e5c26b297ec8bdc
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-05-11 16:45:08 +0800

    Add camera view

---
+++ Assets/Scripts/CamTexture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamTexture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Renderer renderer = GetComponent<Renderer>();
+        WebCamTexture webCamTexture = new WebCamTexture();
+
+        renderer.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
